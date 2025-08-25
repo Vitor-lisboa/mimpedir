c9340cbe4f423c67360c6e66face1a6ba4de6946
Vitor-lisboa <vitorlisboa4998@gmail.com>
2025-08-25 16:30:32 -0300

    Criando telas de home e login com funcionamento básico sem banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'usuario.dart';
+import 'tela_login.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,31 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    Usuario u = Usuario(
-      codigo: 1,
-      login: "VitorLs",
-      nome: "Vitor",
-      senha: "@senhaforte123"
-    );
 
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Meus Dados: ")
-        ),
-        body: Center(
-          child: Padding(padding: const EdgeInsets.all(18.0),
-            child: Column(
-              children: [
-                Text("Nome: ${u.nome}"),
-                Text("Codigo: ${u.codigo}"),
-                Text("Login: ${u.login}"),
-                Text("Senha: ${u.senha}")
-              ],
-            ),
-          ),
-        ),
-      ),
+        debugShowCheckedModeBanner: false,
+        home: TelaLogin(),
     );
   }
 }
