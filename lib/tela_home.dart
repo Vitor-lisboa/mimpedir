98682e4466d2cf4be2c3da31407cfe4a7b408712
Vitor-lisboa <vitorlisboa4998@gmail.com>
2025-09-29 16:25:39 -0300

    colocamos a tela cadastrar restaurante, e mexemos no tela home.

--- lib/tela_home.dart
+++ lib/tela_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mimpedir/tela_cad_restaurante.dart';
 
 class  TelaHome extends StatelessWidget{
   TelaHome({super.key});
@@ -7,9 +8,34 @@ class  TelaHome extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Tela Home')),
-      body: Center(
-        child: Text('Seja bem vindo ao Mim Pedir!'),
+      body: Padding(padding: const EdgeInsets.all(20),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Lista de Restauramentes: "),
+              ElevatedButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => TelaCadRestaurante()));
+              }, child: Text("Cadastrar Restaurante")),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:
+                [
+                  Column(
+                    children: [
+                      Text("Meu restaurante"),
+                      Text("Comida Maxicana")
+                    ],
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(onPressed: (){}, child: Text("Excluir")),
+                  SizedBox(width: 10),
+                  ElevatedButton(onPressed: (){}, child: Text("Editar")),
+
+                ],
+              ),
+            ],
       ),
+    ),
     );
-  }
+}
 }
