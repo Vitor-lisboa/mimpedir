2c10718b982aa7423b8b66c7bab4630e954478a8
Vitor-lisboa <vitorlisboa4998@gmail.com>
2025-10-12 20:16:19 -0300

    Update restaurante.dart
    Fizemos o restaurante.darg

--- lib/restaurante.dart
+++ lib/restaurante.dart
@@ -1,1 +1,12 @@
-// TODO Implement this library.
+import 'usuario.dart';
+import 'tipo.dart';
+
+class Restaurante{
+  int? _codigo;
+  String? _nomeRestaurante;
+  String? _latitude;
+  String? _longitude;
+  Usuario? _proprietario;
+  Tipo? _tipodeCulinaria;
+
+}
