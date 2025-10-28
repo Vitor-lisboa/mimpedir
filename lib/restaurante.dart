5de7fe5587a9769d4816db7980f557553b2cbc60
Vitor-lisboa <vitorlisboa4998@gmail.com>
2025-10-28 11:43:42 -0300

    Update restaurante.dart

--- lib/restaurante.dart
+++ lib/restaurante.dart
@@ -9,4 +9,25 @@ class Restaurante{
   Usuario? _proprietario;
   Tipo? _tipodeCulinaria;
 
+  Restaurante({int? codigo, String? nomeRestaurante,String? latitude, String? longitude, Tipo? tipodeCulinaria, Usuario? proprietario}){
+    _codigo = codigo;
+    _nomeRestaurante = nomeRestaurante;
+    _latitude = latitude;
+    _longitude = longitude;
+    _tipodeCulinaria = tipodeCulinaria;
+    _proprietario = proprietario;
+  }
+  int? get codigo => _codigo;
+  String? get latitude => _latitude;
+  String? get nomeRestaurante => _nomeRestaurante;
+  String? get longitude => _longitude;
+  Tipo? get tipodeCulinaria => _tipodeCulinaria;
+  Usuario? get proprietario=> _proprietario;
+
+  set codigo(int? value) => _codigo = value;
+  set latitude(String? value) => _latitude = value;
+  set nomeRestaurante(String? value) => _nomeRestaurante = value;
+  set longitude(String? value) => _longitude = value;
+  set tipodeCulinaria(Tipo? value) => _tipodeCulinaria = value;
+  set proprietario(Usuario? value) => _proprietario = value;
 }
