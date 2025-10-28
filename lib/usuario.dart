4c9c9a4eb1fbc78f1d8f732fdbb663dd9a7fa7b5
Vitor-lisboa <vitorlisboa4998@gmail.com>
2025-10-28 12:00:29 -0300

    Update usuario.dart

--- lib/usuario.dart
+++ lib/usuario.dart
@@ -1,37 +1,37 @@
 class Usuario{
-    int? _codigo;
-    String? _nome;
-    String? _login;
-    String? _senha;
-
-    Usuario({
-    String? nome,
-    int? codigo,
-    String? senha,
-    String? login}){
-    _senha = senha;
-    _codigo = codigo;
-    _nome = nome;
-    _login = login;
+  //Atributos
+  int? _codigo;
+  String? _nome;
+  String? _login;
+  String? _senha;
+  //Para selecionar classe, privada use ( _ ) antes do nome do atributo
+  //Metodo construtor
+  Usuario({String? nome, int? codigo, String? login, String? senha}){
+   _senha = senha;
+   _codigo = codigo;
+   _nome = nome;
+   _login = login;
   }
-    //getters
-    //escrita do get tradicional
-    String? get login{
-    return _login;
-    }
-    //escrita do get no estilo dart
-    String? get nome => _nome;
-    String? get senha => _senha;
-    int? get codigo => _codigo;
 
-    //setters
-    //escrita do set tradicional
-    set login(String? login){
-      _login = login;
-    }
+  //getters
+  //escrita do get tradicional
+  String? getNome(){
+    return _nome;
+  }
+  //escrita do get no Dart
+  String? get login => _login;
+  String? get senha => _senha;
+  int? get codigo => _codigo;
 
-    //escrita do set do estilo dart
-    set nome(String? nome) => _nome;
-    set senha(String? senha) => _senha;
-    set codigo(int? codigo) => _codigo;
+  //setters
+
+  //escrita do set tradicional
+  set nome(String? nome){
+    _nome = nome;
+  }
+
+  //escrita do set no Dart
+  set login(String? login) => _login = login;
+  set codigo(int? codigo) => _codigo = codigo;
+  set senha(String? senha) => _senha = senha;
 }
